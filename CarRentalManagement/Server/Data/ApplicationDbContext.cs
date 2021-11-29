c0799d2118b4b6ebe7ed22e78f710bcfff3f0e59
Kai Fjellstadsveen <kai@midnightunit.com>
2021-11-29 19:58:12 +0100

    more stuff

--- CarRentalManagement/Server/Data/ApplicationDbContext.cs
+++ CarRentalManagement/Server/Data/ApplicationDbContext.cs
@@ -4,10 +4,6 @@ using CarRentalManagement.Shared.Domain;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Duende.IdentityServer.EntityFramework.Options;
 
 namespace CarRentalManagement.Server.Data
